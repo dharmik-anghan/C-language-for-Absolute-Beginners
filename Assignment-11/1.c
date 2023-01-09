a53b643b36dfe232186e28e82e3fb7a1a70ad514
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-09 15:49:21 +0530

    Update 1.c

--- Assignment-11/1.c
+++ Assignment-11/1.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 
-int LCM(int, int);
 int main()
 {
-    int n1, n2, n;
-    printf("Enter two number: ");
-    scanf("%d%d", &n1, &n2);
-
-    n = LCM(n1, n2);
-    printf("%d", n);
-    return 0;
-}
-
-int LCM(int n1, int n2)
-{
     int n;
-    n = n1>n2?n1:n2;
+    printf("Enter: ");
+    scanf("%d",&n);
 
-    while(1)
-    {
-        if(n%n1 == 0 && n%n2 == 0)
-        {
-            break;
-        }
-        n++;
-    }
-    return n;  
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+        sum += i;
+    
+    printf("Sum of %d natural number is %d.", n, sum);
+
+    return 0;
 }
