1d64309e4298df380f3893a3c8674dbd5eaf962c
Dharmik Anghan <108571957+dharmik-anghan@users.noreply.github.com>
2023-01-09 16:23:36 +0530

    Create 5.c

---
+++ Assignment-8/5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter three number: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    printf("%d", a>b ? a>c?a:c : b>c?b:c);
+
+    return 0;
+
+}
+
+    return 0;
+
+}
